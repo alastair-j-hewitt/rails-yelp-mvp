0ef0349984c5cb667e2fec1c0b501d404427c992
Alastair Hewitt <alastairjohnhewitt@gmail.com>
2019-05-30 11:07:04 -0300

    W6 D4 Quiz

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+ran_res = ["belgian", "chinese", "french", "italian", "japanese"]
+
+# db/seeds.rb
+require 'faker'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::JapaneseMedia::DragonBall.character,
+    address: "#{Faker::Movies::VForVendetta.quote}",
+    category: ran_res.sample,
+    rating: rand(0..5)
+    )
+  restaurant.save!
+end
+
+# 5.times do
+# restaurant = Restaurant.new(
+#   name: "A",
+#   address: "100 Water Street, NY, NYC",
+#   category: "french",
+#   rating: rand(0..5)
+#   )
+# restaurant.save!
+# end
